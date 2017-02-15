88e5e9aee9eb0873bab16b8bacb75577301eabed
Your Name <mhopson@hotmail.co.uk>
2017-02-15 17:53:40 +0000

    Map selcector upgrade

--- Source/Game/Map_GUI.cpp
+++ Source/Game/Map_GUI.cpp
@@ -13,6 +13,12 @@ Map_GUI::Map_GUI()
 {
     m_background.setTexture(&getResources().getTexture(Texture_ID::World_Map));
     m_background.setSize ({Display::WIDTH, Display::HEIGHT});
+
+    m_zones.emplace_back(Zone_ID::Town,
+                         sf::Vector2f{415, 15},
+                         1);
+
+    m_selector.moveTo(m_zones.front());
 }
 
 void Map_GUI::resetBool()
@@ -28,6 +34,10 @@ bool Map_GUI::shouldExit()
 
 void Map_GUI::update()
 {
+    std::cout   << "X: "  << sf::Mouse::getPosition(Display::get()).x
+                << " Y: " << sf::Mouse::getPosition(Display::get()).y
+                << "\n";
+
     m_exitButton.update();
 }
 
@@ -35,4 +45,53 @@ void Map_GUI::draw()
 {
     Display::draw(m_background);
     m_exitButton.draw();
+    m_selector  .draw();
 }
+
+
+Map_Zone::Map_Zone (Zone_ID id, const sf::Vector2f& pos, int lvlReq)
+:   m_id            (id)
+,   m_position      (pos)
+,   m_levelRequied  (lvlReq)
+{ }
+
+Zone_ID Map_Zone::getID() const
+{
+    return m_id;
+}
+
+sf::Vector2f Map_Zone::getPos() const
+{
+    return m_position;
+}
+
+int Map_Zone::getLevelReq() const
+{
+    return m_levelRequied;
+}
+
+
+Zone_Selector::Zone_Selector()
+:   m_selector  (50)
+{
+    m_selector.setFillColor({0, 0, 0, 0});
+    m_selector.setOutlineThickness(3);
+    m_selector.setOutlineColor({255, 0, 0, 150});
+}
+
+void Zone_Selector::moveTo(const Map_Zone& zone)
+{
+    m_p_zone = &zone;
+    m_selector.setPosition(zone.getPos());
+}
+
+void Zone_Selector::draw()
+{
+    Display::draw(m_selector);
+}
+
+const Map_Zone& Zone_Selector::getZone() const
+{
+    return *m_p_zone;
+}
+
--- Source/Game/Map_GUI.h
+++ Source/Game/Map_GUI.h
@@ -1,7 +1,46 @@
 #ifndef MAP_GUI_H_INCLUDED
 #define MAP_GUI_H_INCLUDED
 
+#include <vector>
+
 #include "GUI/Button.h"
+
+ enum class Zone_ID
+ {
+     Town,
+     Forest
+ };
+
+ class Map_Zone
+ {
+     public:
+         Map_Zone(Zone_ID id,
+                  const sf::Vector2f& pos,
+                  int lvlReq);
+
+         Zone_ID getID          () const;
+         sf::Vector2f getPos    () const;
+         int getLevelReq        () const;
+
+     private:
+         Zone_ID         m_id;
+         sf::Vector2f    m_position;
+         int             m_levelRequied;
+ };
+
+ class Zone_Selector
+ {
+     public:
+         Zone_Selector();
+
+         void draw();
+         void moveTo(const Map_Zone& zone);
+         const Map_Zone& getZone() const;
+
+     private:
+         sf::CircleShape m_selector;
+         const Map_Zone* m_p_zone = nullptr;
+ };
 
 class Map_GUI
 {
@@ -14,11 +53,14 @@ class Map_GUI
         void draw();
 
     private:
+        std::vector<Map_Zone> m_zones;
+
+        Zone_Selector m_selector;
+
         sf::RectangleShape m_background;
         GUI::Button m_exitButton;
 
         bool m_shouldExit;
-
 };
 
 #endif // MAP_GUI_H_INCLUDED
